ef010e2e2b36c40c970ab9f4c0c6ee68004ca7dd
Aayannoori15 <Aayan.noori15@gmail.com>
2026-02-02 21:31:48 +0530

    Namespace templates correctly for Django

--- home/views.py
+++ home/views.py
@@ -4,13 +4,13 @@ from home.models import *
 # Create your views here.
 def home(request):
     Project=project.objects.all().order_by('date')
-    return render(request,'home.html',{'project_key':Project})
+    return render(request,'home/home.html',{'project_key':Project})
 def contact(request):
     context={'page':'contact'}
-    return render(request,'contact.html',context=context)
+    return render(request,'home/contact.html',context=context)
 def about(request):
     Experience=experience.objects.all()
     Education=education.objects.all().order_by('-end_date')
     Achievements=achievements.objects.all()
     Certification=certifications.objects.all()
-    return render(request,'about.html',{'experience_key':Experience,'education_key':Education,'Achivement_keys':Achievements,'certification_key':Certification})
+    return render(request,'home/about.html',{'experience_key':Experience,'education_key':Education,'Achivement_keys':Achievements,'certification_key':Certification})
