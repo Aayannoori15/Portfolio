6d2e649bb672c071901368a7ec26e978596978eb
Aayannoori15 <Aayan.noori15@gmail.com>
2026-02-04 21:29:58 +0530

    Cloudinary API

--- personalweb/settings.py
+++ personalweb/settings.py
@@ -90,7 +90,11 @@ else:
         }
     }
 
-
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
 
 
 # Password validation
